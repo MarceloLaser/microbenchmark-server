d1f61ab3594b683886dab020dd3ff44e351bf11d
MarceloLaser <marcelo.laser@gmail.com>
2017-08-16 13:37:27 -0300

    AT LAST

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
 });
 
 /* GET restaurants */
-router.get('/restaurants', function(req, res) {
+router.get('/restaurants/full', function(req, res) {
   var db = req.db;
   var collection = db.get('restaurants');
   collection.find({},{},function(e,docs){
@@ -24,6 +24,26 @@ router.get('/restaurants/:identifier', function(req, res){
   var rid = req.params.identifier;
   collection.find({ "restaurant_id" : rid }, function(err, docs) {
 		if (err){
+      //res.render("i hate io");
+			res.status(400).send(err);
+		}
+		else{
+      // res.render('restaurants', {
+      //   "restaurants" : docs
+      // });
+			res.json(docs);
+		}
+  });
+});
+
+/* GET by NAME and ADDRESS */
+router.get('/restaurants', function(req, res){
+  var db = req.db;
+  var collection = db.get('restaurants');
+  var name = req.query.name;
+  var address = req.query.address;
+  collection.find({ "name" : { "$regex" : name }, "address.street" : { "$regex" : address } }, function(err, docs){
+    if (err){
       //res.render("i hate io");
 			res.status(400).send(err);
 		}
